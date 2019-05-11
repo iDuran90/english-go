c1ef2dec67a70469a87d33375f9a938d2fb97233
Jorge Durán <jdp0990@gmail.com>
2019-05-10 23:15:47 -0500

    Map location (#4)

---
+++ EnglishGo/Assets/Scenes/World/UIManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+  [SerializeField] private Text xpText;
+  [SerializeField] private Text levelText;
+  [SerializeField] private GameObject menu;
+
+  public void Awake() {
+    Assert.IsNotNull(xpText);
+    Assert.IsNotNull(levelText);
+    Assert.IsNotNull(menu);
+  }
+
+  public void updateLevel() {
+    levelText.text = GameManager.Instance.CurrentPlayer.Level.ToString();
+  }
+
+  public void updateXP() {
+    xpText.text = GameManager.Instance.CurrentPlayer.Xp.ToString() + " / " + GameManager.Instance.CurrentPlayer.RequiredXp.ToString();
+  }
+
+  public void toggleMenu(int level) {
+    menu.SetActive(!menu.activeSelf);
+  }
+
+  private void Update()
+  {
+    updateLevel();
+    updateXP();
+  }
+}
---
+++ EnglishGo/Assets/Utilities/PocketDroidConstants.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PocketDroidConstants {
+  public static string SCENE_WORLD = "world";
+  public static string SCENE_CAPTURE = "Capture";
+}
