2de00d9ad9fbeb3a07c543d2db66c139f5a603e8
Jorge Durán <jdp0990@gmail.com>
2019-05-14 23:51:26 -0500

    Teachers location set up

---
+++ EnglishGo/Assets/Models/Teachers/Scripts/Teacher.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teacher : MonoBehaviour {
+  [SerializeField] private string name;
+  [SerializeField] private double latitude = 0;
+  [SerializeField] private double longitude = 0;
+
+  public string Name
+  {
+    get
+    {
+      return name;
+    }
+  }
+
+  public double Latitude
+  {
+    get
+    {
+      return latitude;
+    }
+  }
+
+  public double Longitude
+  {
+    get
+    {
+      return longitude;
+    }
+  }
+
+  private void Start()
+  {
+    DontDestroyOnLoad(this);
+  }
+
+  private void OnMouseDown()
+  {
+    // open camera view with related AR objects
+
+  }
+}
