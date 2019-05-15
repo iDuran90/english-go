23f1555ecad6dac505ffe7455d64e96baaf2d423
Jorge Durán <jdp0990@gmail.com>
2019-05-14 20:27:04 -0500

    Save player data

--- EnglishGo/Assets/Models/Supercyan/Scripts/Player.cs
+++ EnglishGo/Assets/Models/Supercyan/Scripts/Player.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
@@ -8,6 +10,8 @@ public class Player : MonoBehaviour {
   [SerializeField] private int levelBase = 100;
   [SerializeField] private List<GameObject> droids = new List<GameObject>();
   private int level = 1;
+
+  private string path;
 
   public int Xp {
     get { return xp; }
@@ -31,19 +35,64 @@ public class Player : MonoBehaviour {
   public int Level { get { return level; } }
 
   void Start () {
-		
-	}
+    path = Application.persistentDataPath + "/player.dat";
+  }
 	
   public void AddXp(int xp) {
-    this.xp = Mathf.Max(0, xp);
+    this.xp += Mathf.Max(0, xp);
+
+    if (this.xp >= this.requiredXp) {
+      this.xp -= this.requiredXp;
+      this.level++;
+    }
+
+    save();
   }
 
   public void AddDroid(GameObject droid) {
     droids.Add(droid);
+
+    save();
   }
 
   public void InitLevelData() {
     level = (xp / levelBase) + 1;
     requiredXp = levelBase * level;
+  }
+
+  private void initLevelData() {
+    level = (xp / levelBase) + 1;
+    requiredXp = levelBase * level;
+  }
+
+  private void save() {
+    BinaryFormatter bf = new BinaryFormatter();
+    FileStream file = File.Create(path);
+    PlayerData data = new PlayerData(this);
+    bf.Serialize(file, data);
+    file.Close();
+  }
+
+  private void load() {
+    if (File.Exists(path)) {
+      BinaryFormatter bf = new BinaryFormatter();
+      FileStream file = File.Open(path, FileMode.Open);
+      PlayerData data = (PlayerData)bf.Deserialize(file);
+      file.Close();
+
+      xp = data.Xp;
+      requiredXp = data.RequiredXp;
+      levelBase = data.LevelBase;
+      level = data.Level;
+
+      //foreach (DroidData droidData in data.Droids)
+      //{
+      //  Droid droid = new Droid();
+      //  droid.loadFromDroidData(droidData);
+      //  addDroid(droid.gameObject);
+      //}
+    } else {
+      initLevelData();
+    }
   }
 }
